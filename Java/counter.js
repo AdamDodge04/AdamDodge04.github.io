d487264de3872578f6304cc03f66610bb2c74d14
Dodge Adam <815495@stma.k12.mn.us>
2022-04-06 12:46:54 -0500

    More Java Stuff

--- Java/counter.js
+++ Java/counter.js
@@ -8,7 +8,41 @@ function minus() {
 function plus() {
   count = count + 1;
 	document.getElementById("zero").innerHTML = count
+	if (count == 69) {
+		document.getElementById("zero").innerHTML = "NICE:)";
+	}
 }
+function pluss() {
+  count = count + 5;
+	document.getElementById("zero").innerHTML = count
+}
+function plusss() {
+  count = count + 100;
+	document.getElementById("zero").innerHTML = count
+}
+function times() {
+  count = count * 10;
+	document.getElementById("zero").innerHTML = count
+}
+function minusThree() {
+  count = count - 3;
+	document.getElementById("zero").innerHTML = count
+}
+function minusHundo() {
+  count = count - 100;
+	document.getElementById("zero").innerHTML = count
+}
+function divide() {
+  count = count / 2;
+	document.getElementById("zero").innerHTML = count
+}
+
 	
 document.getElementById("minus").addEventListener("click", minus);
 document.getElementById("plus").addEventListener("click", plus);
+document.getElementById("pluss").addEventListener("click", pluss);
+document.getElementById("plusss").addEventListener("click", plusss);
+document.getElementById("times").addEventListener("click", times);
+document.getElementById("minuss").addEventListener("click", minusThree);
+document.getElementById("minusss").addEventListener("click", minusHundo);
+document.getElementById("divide").addEventListener("click", divide);
---
+++ Java/secret.js
@@ -0,0 +1,3 @@
+if (document.getElementById("password").value == "Adams") {
+	document.getElementById("pass").innerHTML = "Welcome"
+}

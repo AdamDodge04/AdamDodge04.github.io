d345458a06057d3363764d6fb042b40ceeef56aa
Dodge Adam <815495@stma.k12.mn.us>
2022-04-28 12:17:23 -0500

    apples

--- Java/appless.js
+++ Java/appless.js
@@ -1,6 +1,67 @@
-function store{
-	window.location "https://www.apple.com/store"
+function home() {
+	window.location = "https://www.apple.com/"
+}
+
+function iPhone13() {
+	window.location = "https://www.apple.com/iphone-13-pro/"
+}
+
+function iPhone1() {
+	window.location = "https://www.apple.com/iphone-13/"
+}
+
+function iPhonese() {
+	window.location = "https://www.apple.com/iphone-se/"
+}
+
+function key() {
+	window.location = "https://www.apple.com/iphone-12/key-features/"
+}
+
+function iPhone11() {
+	window.location = "https://www.apple.com/shop/buy-iphone/iphone-11"
+}
+
+function comp() {
+	window.location = "https://www.apple.com/iphone/compare/"
+}
+
+function air() {
+	window.location = "https://www.apple.com/airpods/"
+}
+
+function tag() {
+	window.location = "https://www.apple.com/airtag/"
+}
+
+function access() {
+	window.location = "https://www.apple.com/shop/iphone/accessories"
+}
+
+function card() {
+	window.location = "https://www.apple.com/apple-card/"
+}
+
+function ios() {
+	window.location = "https://www.apple.com/ios/ios-15/"
+}
+
+function buy() {
+	window.location = "https://www.apple.com/shop/buy-iphone"
 }
 
 
-document.getElementById("store").addEventListener("Click", store)
+
+document.getElementById("home").addEventListener("click", home)
+document.getElementById("13iPhone").addEventListener("click", iPhone13)
+document.getElementById("iPhone1").addEventListener("click", iPhone1)
+document.getElementById("se").addEventListener("click", iPhonese)
+document.getElementById("iphone12").addEventListener("click", key)
+document.getElementById("iphone11").addEventListener("click", iPhone11)
+document.getElementById("compare").addEventListener("click", comp)
+document.getElementById("airpods").addEventListener("click", air)
+document.getElementById("airtag").addEventListener("click", tag)
+document.getElementById("access").addEventListener("click", access)
+document.getElementById("card").addEventListener("click", card)
+document.getElementById("ios").addEventListener("click", ios)
+document.getElementById("shop").addEventListener("click", buy)
